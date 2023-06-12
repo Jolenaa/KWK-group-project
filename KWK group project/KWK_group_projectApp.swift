525605249ccf2d93fd7f2b095a62b01eac7ae9c8
Jolena Akudago <Jolkudago@gmail.com>
2023-06-12 09:49:48 -0600

    Initial Commit

---
+++ KWK group project/KWK_group_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  KWK_group_projectApp.swift
+//  KWK group project
+//
+//  Created by Jolena Akudago on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct KWK_group_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
